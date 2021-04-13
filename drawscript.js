77fd12d333c221043f4a519edec6e81734d45e01
thaone5866 <77510164+thaone5866@users.noreply.github.com>
2021-04-13 10:25:54 -0400

    Create drawscript.js

---
+++ drawscript.js
@@ -0,0 +1,5 @@
+const canvas = document.querySelector('#draw');
+// could be 3d, if you want to make a video game
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
