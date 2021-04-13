ca88c41de0db48862544cda0960c71868fb18a1a
thaone5866 <77510164+thaone5866@users.noreply.github.com>
2021-04-13 11:04:20 -0400

    Update drawscript.js

--- drawscript.js
+++ drawscript.js
@@ -1,10 +1,1 @@
-const canvas = document.querySelector('#draw');
-// could be 3d, if you want to make a video game
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-draw = function() {stroke(29, 143, 19);
-    fill(mouseX, mouseY, 173);
-    ellipse(mouseX, mouseY, 100, 100);
-};
 
